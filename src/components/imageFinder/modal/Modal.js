c3ca6570a740fb30bf61bdb8eba20d3a5bebd162
Ruslana Kravchenko <ruslana.kravchenko24@gmail.com>
2021-01-27 14:53:29 +0200

    Added infinite scroll

---
+++ src/components/imageFinder/modal/Modal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ModalStyled from './ModalStyled';
+
+const Modal = ({ imgURL, imgAlt, onHandleClickInModal }) => {
+  return (
+    <ModalStyled onClick={onHandleClickInModal}>
+      <div className="Modal">
+        <img src={imgURL} alt={imgAlt} />
+      </div>
+    </ModalStyled>
+  );
+};
+export default Modal;
