8bc42f0cc1bc97d9aee30781f7cf0a925de1e522
Ruslana Kravchenko <ruslana.kravchenko24@gmail.com>
2021-02-22 00:06:35 +0200

    Изменено количество подгружаемых картинок

--- src/services/ImagesApi.js
+++ src/services/ImagesApi.js
@@ -4,7 +4,7 @@ const fetchImagesWithQuery = (searchQuery, page = 1) => {
   const key = '19509532-9c10a3c2f413abf8ad2252df8';
   return axios
     .get(
-      `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=12
+      `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=20
 `,
     )
     .then(response => response.data.hits);
